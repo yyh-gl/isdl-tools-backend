0421654cf5ad740ac199df3937819a849f55b952
yyh-gl <yhonda.95.gl@gmail.com>
2018-05-26 02:22:14 +0900

    ssl通信対応

--- api-server/config/puma.rb
+++ api-server/config/puma.rb
@@ -1,3 +1,11 @@
+if "development" == ENV.fetch("RAILS_ENV") { "development" }
+  ssl_bind '0.0.0.0', 443, {
+    key:  ENV['server_key'],
+    cert: ENV['server_crt'],
+    verify_mode: "none"
+  }
+end
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
