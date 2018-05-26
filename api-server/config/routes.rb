219f462afd4e170199806e236b69e3834f9b5351
yyh-gl <yhonda.95.gl@gmail.com>
2018-05-26 14:20:56 +0900

    テキストから感情分析する機能を追加

--- api-server/config/routes.rb
+++ api-server/config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     get '/users', to: 'users#index'
     get '/users/:id', to: 'users#show'
+
+    namespace :google do
+      post '/languages', to: 'languages#analyze'
+    end
   end
 end
