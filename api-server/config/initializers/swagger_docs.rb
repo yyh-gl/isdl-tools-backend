c8446c2c085073c9d42687bd683318c862f51234
yyh-gl <yhonda.95.gl@gmail.com>
2018-05-29 01:09:49 +0900

    Swaggerの設定修正

--- api-server/config/initializers/swagger_docs.rb
+++ api-server/config/initializers/swagger_docs.rb
@@ -7,24 +7,24 @@ end
 
 # 出力JSON設定
 Swagger::Docs::Config.register_apis({
-                                        'v1' => {
-                                            :api_extension_type => nil,
-                                            :api_file_path => 'public/apidocs/', # JSONが置かれるPATH
-                                            :base_path => "http://#{ENV['ip']}:#{ENV['port']}/", # 最後の`/`が置換されてしまうのでURLを記載
-                                            :clean_directory => true,
-                                            :formatting => :pretty,
-                                            :camelize_model_properties => false,
-                                            :controller_base_path => '',
-                                            :attributes => {
-                                                :info => {
-                                                    'title'       => 'Spajam API',
-                                                    'description' => 'for Spajam and you',
-                                                    'contact'     => 'test@example.com',
+                                        #'v1' => {
+                                        :api_extension_type => nil,
+                                        :api_file_path => 'public/apidocs/', # JSONが置かれるPATH
+                                        :base_path => "http://#{ENV['ip']}:#{ENV['port']}/", # 最後の`/`が置換されてしまうのでURLを記載
+                                        :clean_directory => true,
+                                        :formatting => :pretty,
+                                        :camelize_model_properties => false,
+                                        :controller_base_path => '',
+                                        :attributes => {
+                                            :info => {
+                                                'title'       => 'Spajam API',
+                                                'description' => 'for Spajam and you',
+                                                'contact'     => 'test@example.com',
                                                     'license'     => 'Apache 2.0',
-                                                    'licenseUrl'  => 'http://www.apache.org/licenses/LICENSE-2.0.html'
-                                                }
+                                                'licenseUrl'  => 'http://www.apache.org/licenses/LICENSE-2.0.html'
                                             }
                                         }
+                                        #}
                                     })
 
 GrapeSwaggerRails.options.app_name    = "Spajam API"
