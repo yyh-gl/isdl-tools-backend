b6425de27093a93516e9fb91ae00713a937d60a3
yyh-gl <yhonda.95.gl@gmail.com>
2018-05-29 02:03:07 +0900

    リファクタリング

--- api-server/config/initializers/swagger_docs.rb
+++ api-server/config/initializers/swagger_docs.rb
@@ -7,7 +7,7 @@ end
 
 # 出力JSON設定
 Swagger::Docs::Config.register_apis(
-    {
+    { 'v1' => {
         :api_extension_type => nil,
         :api_file_path => 'public/apidocs/', # JSONが置かれるPATH
         :base_path => "http://#{ENV['ip']}:#{ENV['port']}/", # 最後の`/`が置換されてしまうのでURLを記載
@@ -24,7 +24,7 @@ Swagger::Docs::Config.register_apis(
                 'licenseUrl'  => 'http://www.apache.org/licenses/LICENSE-2.0.html'
             }
         }
-    })
+    }})
 
 GrapeSwaggerRails.options.app_name    = "Spajam API"
 # 基盤となるJSON
