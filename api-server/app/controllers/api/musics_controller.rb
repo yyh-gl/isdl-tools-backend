974a362acfa1f23bfc54a4ca0aad628f57caa071
yyh-gl <yhonda.95.gl@gmail.com>
2018-06-16 16:33:43 +0900

    swagger設定追加

--- api-server/app/controllers/api/musics_controller.rb
+++ api-server/app/controllers/api/musics_controller.rb
@@ -1,8 +1,8 @@
 class Api::MusicsController < ApplicationController
   # GET /api/music
   def index
-    users = Music.all
-    json_response(users)
+    Musics = Music.all
+    json_response(Musics)
   end
 
   # POST /api/music
@@ -15,5 +15,38 @@ class Api::MusicsController < ApplicationController
   def show
     user = Music.find(params[:id])
     json_response(user)
+  end
+
+  swagger_controller :Musics, 'Music API'
+
+  swagger_api :index do
+    summary 'Get all Musics information'
+    consumes ['application/json']
+    response :ok, 'Success', :Music
+    response :not_found
+    response :internal_server_error
+  end
+
+  swagger_api :create do
+    summary 'Add new Music information'
+    consumes ['application/json']
+    param :query, :name, :string, :required, 'Music name'
+    param :query, :artist, :string, :required, 'Artist name'
+    param :query, :path, :string, :required, 'Path to music'
+    response :ok, 'Success', :Music
+    response :internal_server_error
+  end
+
+  swagger_api :show do
+    summary 'Get a Music information'
+    consumes ['application/json']
+    param :path, :id, :integer, :required, 'Music Id'
+    response :ok, 'Success', :Music
+    response :not_found
+    response :internal_server_error
+  end
+
+  swagger_model :Music do
+    description 'Music parameters'
   end
 end
