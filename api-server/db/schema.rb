791355c4c2717846c8e913cbe60fb29d2f5b796d
yyh-gl <yhonda.95.gl@gmail.com>
2018-06-16 15:08:06 +0900

    Userテーブル削除

--- api-server/db/schema.rb
+++ api-server/db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180525151857) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.text "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180616060655) do
 
 end
