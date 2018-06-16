58da196cebc8158910023efb16809c9d0bdad9c5
yyh-gl <yhonda.95.gl@gmail.com>
2018-06-16 17:45:42 +0900

    ユーザのアップロードした曲取得API完成

--- api-server/app/controllers/api/musics_controller.rb
+++ api-server/app/controllers/api/musics_controller.rb
@@ -1,20 +1,27 @@
 class Api::MusicsController < ApplicationController
-  # GET /api/music
+  # GET /api/musics
   def index
-    music = Music.all
-    json_response(music)
+    musics = Music.all
+    json_response(musics)
   end
 
-  # POST /api/music
+  # POST /api/musics
   def create
     music = Music.create!(name: params[:name], artist: params[:artist], path: params[:path])
     json_response(music)
   end
 
-  # GET /api/music/:id
+  # GET /api/musics/:id
   def show
     music = Music.find(params[:id])
     json_response(music)
+  end
+
+  # GET /api/musics/user/:id
+  def user_index
+    user = User.find(params[:id])
+    musics = user.musics
+    json_response(musics)
   end
 
   swagger_controller :Musics, 'Music API'
@@ -41,6 +48,15 @@ class Api::MusicsController < ApplicationController
     summary 'Get a Music information'
     consumes ['application/json']
     param :path, :id, :integer, :required, 'Music Id'
+    response :ok, 'Success', :Music
+    response :not_found
+    response :internal_server_error
+  end
+
+  swagger_api :user_index do
+    summary 'Get a Users music information'
+    consumes ['application/json']
+    param :path, :id, :integer, :required, 'User id'
     response :ok, 'Success', :Music
     response :not_found
     response :internal_server_error
